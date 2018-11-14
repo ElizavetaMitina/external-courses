67f3deadc7c8d05352bab2cb91cea9037d34e428
Liza Mitina <tim445406@gmail.com>
2018-11-14 09:25:36 +0400

    Fixed

--- src/ex12_js-DOM_events/task-02.js
+++ src/ex12_js-DOM_events/task-02.js
@@ -3,9 +3,9 @@ document.getElementById('Wrapper').addEventListener('click', toggleItem);
 function toggleItem(e) {
     if (e.target && e.target.nodeName === 'H2') {
         let parentItem = e.target.parentNode;
-        for (let i = 0; i < accItem.length; i++) {
-            accItem[i].className = 'Item close';
-        }
+        let open = document.getElementsByClassName('Item open')[0];
+        open.classList.remove('open');
+        open.classList.add('close');
         if (parentItem.className === 'Item close') {
             parentItem.className = 'Item open';
         }
